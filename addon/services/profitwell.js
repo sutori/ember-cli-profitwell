236eedd3f6ca4f1030f826033e560c8f4091e621
Yoran Brondsema <yoran@sutori.com>
2017-06-26 13:40:02 -0500

    Check for existence of profitwell script

--- addon/services/profitwell.js
+++ addon/services/profitwell.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   setUserEmail(email) {
-    window.profitwell('user_email', email);
+    if (window.profitwell) {
+      window.profitwell('user_email', email);
+    }
   }
 });
