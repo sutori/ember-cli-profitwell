493444ae5c2220d72126614db32fd73166fd59a9
Yoran Brondsema <yoran@sutori.com>
2018-03-22 19:04:18 +0100

    Fix tests

--- tests/acceptance/content-for-test.js
+++ tests/acceptance/content-for-test.js
@@ -1,24 +1,24 @@
-import { test } from 'qunit';
-import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
+import { visit } from '@ember/test-helpers';
 
-moduleForAcceptance('Acceptance | content for');
+module('Acceptance | content for', function(hooks) {
+  setupApplicationTest(hooks);
 
-const SCRIPT_BODY = `(function(i,s,o,g,r,a,m){i['ProfitWellObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m);
-      })(window,document,'script','https://dna8twue3dlxq.cloudfront.net/js/profitwell.js','profitwell');`;
+  const PART_OF_SCRIPT = `(function(i,s,o,g,r,a,m){i['ProfitWellObject']=r;i[r]=i[r]||function(){`;
 
-test('the recording script is inserted into the document head', async function(assert) {
-  await visit('/');
+  test('the recording script is inserted into the document head', async function(assert) {
+    await visit('/');
 
-  const body = document.getElementsByTagName('body')[0].innerHTML;
+    const body = document.getElementsByTagName('body')[0].innerHTML;
 
-  assert.ok(
-    body.includes(SCRIPT_BODY),
-    'script body is included'
-  );
-  assert.ok(
-    body.includes(`profitwell('auth_token', 'the-token');`),
-    'includes auth_token value'
-  );
+    assert.ok(
+      body.includes(PART_OF_SCRIPT),
+      'script is included'
+    );
+    assert.ok(
+      body.includes(`profitwell('auth_token', 'the-token');`),
+      'includes auth_token value'
+    );
+  });
 });
--- tests/unit/services/profitwell-test.js
+++ tests/unit/services/profitwell-test.js
@@ -1,16 +1,19 @@
-import { moduleFor } from 'ember-qunit';
+import { module } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import test from 'ember-sinon-qunit/test-support/test';
 
-moduleFor('service:profitwell', 'Unit | Service | Profitwell');
+module('Unit | Service | Profitwell', function(hooks) {
+  setupTest(hooks);
 
-test('#setUserEmail', function(assert) {
-  const profitwellSpy = this.spy(window, 'profitwell');
-  const service = this.subject();
+  test('#setUserEmail', function(assert) {
+    const profitwellSpy = this.spy(window, 'profitwell');
+    const service = this.owner.lookup('service:profitwell');
 
-  service.setUserEmail('someone@somewhere.com');
+    service.setUserEmail('someone@somewhere.com');
 
-  assert.ok(
-    profitwellSpy.calledWithExactly('user_email', 'someone@somewhere.com'),
-    'It sets the user_email on the profitwell object'
-  );
+    assert.ok(
+      profitwellSpy.calledWithExactly('user_email', 'someone@somewhere.com'),
+      'It sets the user_email on the profitwell object'
+    );
+  });
 });
